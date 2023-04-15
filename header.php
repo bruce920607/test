1cd1b79ea53d4e1d37e8da500b21b323b125d03c
BCBruce <30749135+bruce920607@users.noreply.github.com>
2023-04-16 00:55:24 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    	<link href="./plugins/pg-calendar/css/pignose.calendar.min.css" rel="stylesheet">
+    	<link rel="stylesheet" href="./plugins/chartist/css/chartist.min.css">
+    	<link rel="stylesheet" href="./plugins/chartist-plugin-tooltips/css/chartist-plugin-tooltip.css">
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    	<title>馬會貨幣GM系統</title>
+    </head>
---
+++ test_page.php
@@ -0,0 +1,77 @@
+<?php include("header.php");?>
+
+<body>
+		<div class="content-body">
+			<div class="container-fluid mt-3">
+				<div class="row">
+					<div class="col-lg-4 col-sm-4">
+						<div class="card gradient-2">
+							<div class="card-body">
+								<h3 class="card-title text-white">政府持有總額</h3>
+								<div class="d-inline-block">
+									<h2 class="text-white">100</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-sm-4">
+						<div class="card gradient-3">
+							<div class="card-body">
+								<h3 class="card-title text-white">社員持有總額</h3>
+								<div class="d-inline-block">
+									<h2 class="text-white">0</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-sm-4">
+						<div class="card gradient-4">
+							<div class="card-body">
+								<h3 class="card-title text-white">國庫持有總額</h3>
+								<div class="d-inline-block">
+									<h2 class="text-white">100</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6 col-sm-8">
+						<div class="card gradient-8">
+							<div class="card-body">
+								<h3 class="card-title text-white">真神幣發行總額</h3>
+								<div class="d-inline-block">
+									<h2 class="text-white">0</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6 col-sm-8">
+						<div class="card gradient-9">
+							<div class="card-body">
+								<h3 class="card-title text-white">帳戶總數量</h3>
+								<div class="d-inline-block">
+									<h2 class="text-white">0</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+        			<div class="col-lg-12">
+						<div class="card">
+							<div class="card-body pb-0 d-flex justify-content-between">
+				    			<div>
+                        			<h4 class="mb-1">排行榜</h4>
+                        			<p>TOP 10</p>
+                    			</div>
+							</div>
+                        </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+
+
+<?php include("footer.php");?>
